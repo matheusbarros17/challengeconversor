6cf5c8e20988d74b9bf94e0474939b9713f4e2ae
matheusbarros17 <matheuscarvalho_5@hotmail.com>
2025-06-22 12:11:01 -0300

    construindo o menu

--- src/br/com/alura/challenge/principal/PrincipalConversor.java
+++ src/br/com/alura/challenge/principal/PrincipalConversor.java
@@ -4,6 +4,16 @@ public class PrincipalConversor {
 
     public static void main(String[] args) {
 
-        System.out.println("Seja bem-vindo");
+        System.out.println("Seja bem-vindo/a ao Conversor de Moedas");
+
+        System.out.println("\n1) Dólar =>> Peso argentino" +
+                "\n2) Peso argetino ==> Dólar" +
+                "\n3) Dólar =>> Real brasileiro" +
+                "\n4) Real brasileiro ==> Dólar" +
+                "\n5) Dólar ==> Peso colombiano" +
+                "\n6) Peso colombiano ==> Dólar" +
+                "\n7) Sair" +
+                "\nEscolha uma opção válida:" +
+                "\n*********************************************************");
     }
 }
